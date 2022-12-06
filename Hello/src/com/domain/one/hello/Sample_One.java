9334e8dce44b306eab8a51f5f76260fa84f317b1
guruprasad_s <guruprasad_s@LT2572.solartistech.local>
2022-12-06 12:38:37 +0530

    okkkkkkkkkkkk

--- Hello/src/com/domain/one/hello/Sample_One.java
+++ Hello/src/com/domain/one/hello/Sample_One.java
@@ -3,7 +3,7 @@ package com.domain.one.hello;
 public class Sample_One {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Abi, Guru and Karthiga");
+		System.out.println("Hello Abi, Guru and Karthiga  OKay");
 	}
 
 }
